7591388e5b23ec1e3e195c580a79c37940138593
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-09-30 19:43:17 -0300

    feat: Capturando Elemento pelo Id

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Selecionando o div pelo ID
+let div =
+    document.getElementById('meuDiv');
+
+//Selecionando todos os parágrafos com a classe "paragrafo"
+let paragrafos =
+    document.getElementsByClassName('paragrafo');
+
+//Selecionando o primeiro parágrafo com a classe "paragrafo"
+let primeiroParagrafo =
+    document.querySelector('.paragrafo');
+
+//Selecionando todos os parágrafos dentro do div
+let paragrafosDentroDiv =
+    document.querySelectorAll('#meuDiv p');
