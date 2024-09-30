978029698716f3734100a3dd27848c69b7695c44
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-09-30 20:13:54 -0300

    feat: Testando função "aoClicar"

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-//Selecionando o div pelo ID
-let div =
-    document.getElementById('meuDiv');
+// //Selecionando o div pelo ID
+// let div =
+//     document.getElementById('meuDiv');
 
-//Selecionando todos os parágrafos com a classe "paragrafo"
-let paragrafos =
-    document.getElementsByClassName('paragrafo');
+// //Selecionando todos os parágrafos com a classe "paragrafo"
+// let paragrafos =
+//     document.getElementsByClassName('paragrafo');
 
-//Selecionando o primeiro parágrafo com a classe "paragrafo"
-let primeiroParagrafo =
-    document.querySelector('.paragrafo');
+// //Selecionando o primeiro parágrafo com a classe "paragrafo"
+// let primeiroParagrafo =
+//     document.querySelector('.paragrafo');
 
-//Selecionando todos os parágrafos dentro do div
-let paragrafosDentroDiv =
-    document.querySelectorAll('#meuDiv p');
+// //Selecionando todos os parágrafos dentro do div
+// let paragrafosDentroDiv =
+//     document.querySelectorAll('#meuDiv p');
 
-//fim do exemplo
+// //fim do exemplo
+
+// Funções para os eventos
+function aoClicar() {
+    alert('Você clicou no Botão!');
+}
+function aoPerderFoco() {
+    alert('Você saiu do campo!');
+}
+// Selecionando o elemento usando querySelector
+let campo = document.querySelector('#meuCampo');
+let botao = document.querySelector('.btnCampo');
+
+// Adicionando os eventos
+campo.addEventListener('blur', aoPerderFoco);
+botao.addEventListener('click', aoClicar);
