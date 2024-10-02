beb2153cd81e1e39cc48d89aa5bc6d3284ccf33e
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-01 21:08:41 -0300

    feat: Ex 4 corrigido

--- exercicios/Ex4/js/script.js
+++ exercicios/Ex4/js/script.js
@@ -6,3 +6,30 @@
 // 5. Quinta-feira
 // 6. Sexta-feira
 // 7. Sábado
+
+function exibirSemana() {
+    let dSemana = document.querySelector("#diaSemana").ariaValueMax;
+    let resultado = "";
+    if(dSemana == "1")
+        resultado = "Domingo";
+    else if (dSemana == "2")
+        resultado = "Segunda-feira";
+    else if (dSemana == "3")
+        resultado = "Terça-feira";
+    else if (dSemana == "4")
+        resultado = "Quarta-feira";
+    else if (dSemana == "5")
+        resultado = "Quinta-feira";
+    else if (dSemana == "6")
+        resultado = "Sexta-feira";
+    else if (dSemana == "7")
+        resultado = "Sábado";
+    else
+        resulta = "Invalido"
+
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = resultado;
+}
+
+document.querySelector('button')
+.addEventListener('click', exibirSemana);
