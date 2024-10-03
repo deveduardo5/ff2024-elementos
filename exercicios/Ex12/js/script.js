6fa4418ab191301280c227d7b4331f004b840240
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-03 20:58:33 -0300

    feat: Ex 12 corrigido

--- exercicios/Ex12/js/script.js
+++ exercicios/Ex12/js/script.js
@@ -1,15 +1,40 @@
-// Faça um algoritmo que pergunte o nome do usuário, o quanto ganha por hora e o número de horas
-// trabalhadas no mês. Calcule o resultado e mostre o nome e o total da renda recebida no mês.
+// Faça um algoritmo para montar uma calculadora simples, onde o usuário vai optar em escolher conforme o menu 
+// adiante. 
+// Observação: no caso de divisão, o denominador não pode ser zero, invalidando a operação.
+// 1. Soma.
+// 2. Subtração.
+// 3. Multiplicação.
+// 4. Divisão
+// 5. Potenciação
+// 6. Radiciação
 
-function calcularResultado() {
-    let nome = document.querySelector('#nome').value;
-    let valoHora = parseFloat(document.querySelector('#valorHora').value);
-    let quantHora = parseFloat(document.querySelector('#quantHora').value);
+function exibirResultado() {
+    let num1 = parseFloat(document.querySelector("#num1").value);
+    let num2 = parseFloat(document.querySelector("#num2").value);
+    let operacao = document.querySelector('select').value;
 
-    let resultado = valoHora * quantHora;
-
-    document.querySelector("#resultado").innerHTML =
-        `O valor recebido do trabalhador ${nome} é R$ ${resultado.toFixed(2)}`;
+    switch (operacao) {
+        case "1":
+            document.querySelector('#resultado').innerHTML = ` A soma é ${num1 + num2}`;
+            break;
+        case "2":
+            document.querySelector('#resultado').innerHTML = ` A subtração é ${num1 - num2}`;
+            break;
+        case "3":
+            document.querySelector('#resultado').innerHTML = ` A multiplicação é ${num1 * num2}`;
+            break;
+        case "4":
+            document.querySelector('#resultado').innerHTML = ` A divisão é ${num1 / num2}`;
+            break;
+        case "5":
+            document.querySelector('#resultado').innerHTML = `A potenciação é ${num1 ** num2}`;
+            break;
+        case "6":
+            document.querySelector('#resultado').innerHTML = ` A radiciação é ${num1 ** (1/num2)}`;
+            break;
+    }
 }
 
-document.querySelector('.btn').addEventListener('click',calcularResultado);
+document
+    .querySelector('.btn')
+    .addEventListener('click',exibirResultado);
