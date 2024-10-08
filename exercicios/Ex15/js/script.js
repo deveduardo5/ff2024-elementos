d35fa32658c27b9138a231da5897be6144aed9b6
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-07 21:24:46 -0300

    feat: Exercício 15 com Bootstrap

--- exercicios/Ex15/js/script.js
+++ exercicios/Ex15/js/script.js
@@ -1,23 +1,25 @@
-// Faça um algoritmo que leia um nome de usuário e a sua senha. A senha não pode ser igual ao
-// nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
 
-function verificarAutenticacao() {
+/*Faça um algoritmo que leia um nome de usuário e a sua senha. 
+A senha não pode ser igual ao nome do usuário, 
+mostrando uma mensagem de erro e voltando a pedir as informações. 
+*/
+
+function verificarLogin() {
     let login = document.querySelector('#login').value;
     let senha = document.querySelector('#senha').value;
 
     if (login === senha) {
-        alert("Digite Novamente a Senha!");
-        document.querySelector('body').style = "backgroundColor = red;";
+        alert("A senha não deve ser igual ao login!");
+        document.querySelector('body').style = "background-color: red;";
         //document.querySelector('body').style.backgroundColor = "red";
         document.querySelector('#login').value = "";
         document.querySelector('#senha').value = "";
+
     }
     else{
-        alert("Logado com sucesso!")
+        alert("Logado com sucesso!");
     }
 
 }
-
-document
-    .querySelector('.btn')
-    .addEventListener('click', verificarAutenticacao);
+document.querySelector('button')
+    .addEventListener('click', verificarLogin)
