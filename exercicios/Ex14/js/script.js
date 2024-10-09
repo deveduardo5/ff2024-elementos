3a6f156c68000c0f8e2319c4b3e5c3ea9db341a3
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-08 21:49:31 -0300

    feat: Ex 14 corrigido

--- exercicios/Ex14/js/script.js
+++ exercicios/Ex14/js/script.js
@@ -1,15 +1,39 @@
-// Faça um algoritmo que pergunte o nome do usuário, o quanto ganha por hora e o número de horas
-// trabalhadas no mês. Calcule o resultado e mostre o nome e o total da renda recebida no mês.
+// Faça um algoritmo que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e 
+// unidades do mesmo. Observando os termos no plural a colocação do "e", da vírgula entre outros.
+// Exemplo: 326: 3 centenas, 2 dezenas e 6 unidades
 
-function calcularResultado() {
-    let nome = document.querySelector('#nome').value;
-    let valoHora = parseFloat(document.querySelector('#valorHora').value);
-    let quantHora = parseFloat(document.querySelector('#quantHora').value);
+function gerarTabuada() {
+    let numero = document.querySelector('#numero').value;
 
-    let resultado = valoHora * quantHora;
+    if (isNaN(numero) == true || numero == "") {
+        alert("Número Inválido!");
+        document.querySelector('#numero').value = "";
+    } else {
+        let msgTabuada = "";
+        for (let i = 1; i <= 10; i++) {
+            msgTabuada += `${numero} X ${i} = ${numero * i} <br>`;
+        }
+        document.querySelector('h5').innerHTML = msgTabuada;
+        document.querySelector('#numero').disabled = true;
+        document.querySelector('.btn-primary').disabled = true;
+        document.querySelector('.btn-dark').disabled = false;
+    }
 
-    document.querySelector("#resultado").innerHTML =
-        `O valor recebido do trabalhador ${nome} é R$ ${resultado.toFixed(2)}`;
+
 }
+function liberarTabuada() {
+    let podeGerar = confirm("Deseja gerar outra tabuada?");
 
-document.querySelector('.btn').addEventListener('click',calcularResultado);
+    if (podeGerar == true) {
+        document.querySelector('#numero').disabled = false;
+        document.querySelector('.btn-primary').disabled = false;
+        document.querySelector('.btn-dark').disabled = true;
+    }
+}
+document
+    .querySelector('.btn-primary')
+    .addEventListener('click', gerarTabuada);
+
+document
+    .querySelector('.btn-dark')
+    .addEventListener('click', liberarTabuada);
