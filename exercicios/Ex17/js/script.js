06485064008c8297c9e27a6af89be83926708578
deveduardo5 <eduardo.asouza23@senacsp.edu.br>
2024-10-08 21:13:02 -0300

    feat: revisando ex 17

--- exercicios/Ex17/js/script.js
+++ exercicios/Ex17/js/script.js
@@ -5,30 +5,25 @@ Tabuada de 5:
 5 x 1 = 5
 5 x 2 = 10
 ...
-5 x 10 = 50 
+50 = R$ 12,50
 */
 function gerarTabuada() {
     let numero = document.querySelector('#numero').value;
-
     if (isNaN(numero) == true || numero == "") {
         alert("Número Inválido!");
         document.querySelector('#numero').value = "";
     } else {
         let msgTabuada = "";
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= 10; i++) 
             msgTabuada += `${numero} X ${i} = ${numero * i} <br>`;
-        }
         document.querySelector('h5').innerHTML = msgTabuada;
         document.querySelector('#numero').disabled = true;
         document.querySelector('.btn-primary').disabled = true;
         document.querySelector('.btn-dark').disabled = false;
     }
-
-
 }
 function liberarTabuada() {
     let podeGerar = confirm("Deseja gerar outra tabuada?");
-
     if (podeGerar == true) {
         document.querySelector('#numero').disabled = false;
         document.querySelector('.btn-primary').disabled = false;
@@ -38,7 +33,6 @@ function liberarTabuada() {
 document
     .querySelector('.btn-primary')
     .addEventListener('click', gerarTabuada);
-
 document
     .querySelector('.btn-dark')
     .addEventListener('click', liberarTabuada);
--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-/*
-Desenvolva um gerador de tabuada, capaz de gerar através de qualquer número inteiro, 
-solicitando ao usuário o número e mostrando na tela conforme o exemplo abaixo:
-Tabuada de 5:
-5 x 1 = 5
-5 x 2 = 10
-...
-50 = R$ 12,50
-*/
+// O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar a metodologia 
+// da tabelinha. Você foi contratado para desenvolver o programa que monta a tabela de preços 
+// de pães, de 1 até 50 pães, a partir do preço do pão informado pelo usuário, conforme o 
+// exemplo abaixo:
+// Preço do pão: R$ 0,25 unid. 
+//  1 = R$ 0,25
+//  2 = R$ 0,50 
+// ...
+// 50 = R$ 12,50
+
 function gerarTabuada() {
     let numero = document.querySelector('#numero').value;
     if (isNaN(numero) == true || numero == "") {
